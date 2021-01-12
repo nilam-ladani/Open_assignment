7fb09fbf8d3f9bac218394a53e89b038d425f03d
Nilamben Gauravkumar Ladani <nilamben.ladani@tenrglobal.com>
2021-01-12 20:45:50 +0530

    Open: Completed assignment in Angular following wire-frames

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'open';
+  // name = 'Slider Example';
+  value = 0;
+  min=10000;
+max=700000;
+  step = 10;
+  value2 = 0;
+  min2=10000;
+max2=700000;
+  step2 = 10;
+  // thumbLabel = true;
+  calculatorForm = this.fb.group({
+    monthlyIncome: [],
+    monthlyExpense: [],
+    tenure: []
+  });
+  constructor(
+    private fb: FormBuilder
+  ){}
+  submitForm(){
+    
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatSliderModule } from '@angular/material/slider';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatInputModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatSliderModule
   ],
   providers: [],
   bootstrap: [AppComponent]
